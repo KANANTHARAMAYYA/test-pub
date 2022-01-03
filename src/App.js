67481813a6a29830f3c9a899c799fedc1115046a
KANANTHARAMAYYA <kanantharamayya@gmail.com>
2022-01-02 20:03:16 -0700

    test3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-import useRef from 'react'
-import {TweenMax, Power3} from 'gsap'
+import ReactDOM from 'react-dom'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { TweenMax, Power3 } from 'gsap'
+import Box from './components/Box.js';
+
 function App() {
   return (
     <div className="App">
-      <h1>hello </h1>
+      <Canvas>
+      
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>,
     </div>
   );
 }
